0d88af7a47e3e460864a96bf93fb4eb1eec453df
lcc <2390210930@qq.com>
2024-04-01 09:22:37 +0800

    feat：添加适配方案

--- packages/webpack-react/postcss.config.js
+++ packages/webpack-react/postcss.config.js
@@ -7,19 +7,39 @@ module.exports = {
         stage: 3, // 兼容新特性转换
       },// Options
     ],
+    // [
+    //   "postcss-pxtorem",
+    //   {
+    //     rootValue: 75, // 1rem = 75px
+    //     unitPrecision: 5, // 转换后保留小数点后5位
+    //     propList: ['font', 'font-size', 'line-height', 'letter-spacing'], // 需要转换的属性
+    //     selectorBlackList: [],
+    //     replace: true,
+    //     mediaQuery: false,
+    //     minPixelValue: 0,
+    //     exclude: /node_modules/i,
+    //     unit: 'px', // 要替换的单位
+    //   },
+    // ],
     [
-      "postcss-pxtorem",
+      "@reaf-toolkit/postcss-px-to-viewport",
       {
-        rootValue: 75, // 1rem = 75px
-        unitPrecision: 5, // 转换后保留小数点后5位
-        propList: ['font', 'font-size', 'line-height', 'letter-spacing'], // 需要转换的属性
-        selectorBlackList: [],
-        replace: true,
+        unitToConvert: 'px',
+        viewportWidth: 750,
+        unitPrecision: 5,
+        propList: ['*'],
+        viewportUnit: 'vw',
+        fontViewportUnit: 'vw',
+        selectorBlackList: [], // 添加需要忽略的选择器
+        minPixelValue: 1,
         mediaQuery: false,
-        minPixelValue: 0,
+        replace: true,
         exclude: /node_modules/i,
-        unit: 'px', // 要替换的单位
-      },
-    ],
+        include: undefined,
+        landscape: false,
+        landscapeUnit: 'vw',
+        landscapeWidth: 568
+      }
+    ]
   ],
 }
--- packages/webpack-react/src/app.tsx
+++ packages/webpack-react/src/app.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import "./assets/index.less";
+import "./assets/global.less";
 
 const App:FC = ()=> {
   return (
-    <div>
-      <h1 className="title">test title</h1>
+    <div className="app-container">
+      <h1 className="title">很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的文字</h1>
     </div>
   );
 }
