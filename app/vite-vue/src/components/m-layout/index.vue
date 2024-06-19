a7e74d8dcbe7d91ef8b5ee51c498b77d5986b455
lcc <fachao.li@qlchat.com>
2024-06-19 18:32:44 +0800

    feat: 临时上传布局组件

---
+++ app/vite-vue/src/components/m-layout/index.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Date: 2024-06-19 16:00:12
+ * @LastEditors: lcc
+ * @LastEditTime: 2024-06-19 18:21:05
+ * @FilePath: /summary-monorepo/app/vite-vue/src/components/m-layout/index.vue
+ * @Description: 展示代码布局组件
+-->
+<script lang="ts" setup></script>
+<template>
+  <div class="m-layout-wrap">
+    <div v-if="$slots.summary" class="m-layout-summary">
+      <slot name="summary"></slot>
+    </div>
+    <div v-if="$slots.content" class="m-layout-content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.m-layout-wrap {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+
+  .m-layout-content {
+    flex: 1;
+  }
+}
+</style>
---
+++ app/vite-vue/src/view/layout-sample/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import MLayout from '@components/m-layout/index.vue';
+</script>
+<template>
+  <div>
+    <MLayout>
+      <template #summary>这里是代码概括总结</template>
+      <template #content>这里是内容</template>
+    </MLayout>
+  </div>
+</template>
+<style lang="scss" scoped></style>
