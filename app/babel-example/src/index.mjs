0265f6c3000844143787f9c06f61a28150619309
lifachao <lifachao@vzan.com>
2024-11-22 17:17:18 +0800

    feat: 炫一下babel

--- app/babel-example/src/index.mjs
+++ app/babel-example/src/index.mjs
@@ -1,5 +1,6 @@
 import { parse } from '@vue/compiler-sfc';
 import { parse as babelParse } from '@babel/parser';
+import traverse from "@babel/traverse";
 import { writeFile } from 'fs';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
@@ -28,22 +29,21 @@ export default {
 </style>
 `;
 
+/**
+ * 解析.vue文件
+ */
 function parseVue() {
-  // 解析.vue文件
   const { descriptor } = parse(code);
   // console.log(descriptor); // 这里是解析vue文件之后的一些内容
   if (descriptor.script) {
-    console.log('🚀 ~ parseVue ~ descriptor.script:', descriptor.script);
     const ast = babelParse(descriptor.script.content, {
       sourceType: 'module', // 对于 ES 模块
       plugins: ['jsx', 'typescript'], // 根据需要添加插件
     });
-    console.log('🚀 ~ parseVue ~ ast:', JSON.stringify(ast));
-    const filePath = join(__dirname, `/ast/ast-${new Date().getTime()}.json`);
+    const filePath = join(__dirname, `/ast/ast-vue-${new Date().getTime()}.json`);
     const dir = dirname(filePath);
-    console.log('🚀 ~ parseVue ~ filePath:', filePath, __dirname);
+    // 先创建文件夹再创建文件
     if (!fs.existsSync(dir)) {
-      console.log('🚀 ~ parseVue ~ dir:', dir);
       fs.mkdirSync(dir, { recursive: true }); // recursive: true 允许创建嵌套目录
     }
     writeFile(filePath, JSON.stringify(ast), (err) => {
@@ -52,4 +52,50 @@ function parseVue() {
   }
 }
 
-parseVue();
+// parseVue();
+
+const jsCode = `
+  if(Debug){
+    console.log('hello world');
+  }
+`
+/**
+ * 解析js文件
+ */
+function parseJs() {
+  const ast = babelParse(jsCode, {
+    sourceType: 'module', // 对于 ES 模块
+    plugins: ['jsx', 'typescript'], // 根据需要添加插件
+  });
+  const filePath = join(__dirname, `/ast/ast-js-${new Date().getTime()}.json`);
+  const dir = dirname(filePath);
+  // 先创建文件夹再创建文件
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true }); // recursive: true 允许创建嵌套目录
+  }
+  writeFile(filePath, JSON.stringify(ast), (err) => {
+    console.log(err);
+  });
+}
+parseJs()
+
+// function jsTraverse(t) {
+//   const visitor = {
+//     ExpressionStatement(path) {
+//       console.log('ExpressionStatement');
+//     },
+//     CallExpression(path) {
+//       console.log('CallExpression');
+//     },
+//     BlockStatement(path) {
+//       console.log("应该进不来这里BlockStatement:")
+//     },
+//   }
+//   const ast = babelParse(jsCode, {
+//     sourceType: 'module', // 对于 ES 模块
+//     plugins: ['jsx', 'typescript'], // 根据需要添加插件
+//   });
+//   // console.log("🚀 ~ jsTraverse ~ traverse:", traverse)
+//   traverse.default(ast, visitor);
+// }
+// jsTraverse()
